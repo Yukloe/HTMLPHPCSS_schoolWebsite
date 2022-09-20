91d3f6ee5ac5620c6fdf2e97636f07ce10d72a03
Hugo Patry <hugo.patry@groupe-esigelec.org>
2022-09-20 22:19:29 +0200

    Connexion in progress

--- tt_connexion.php
+++ tt_connexion.php
@@ -5,18 +5,34 @@
   $email =  htmlentities($_POST['email']);
   $password = htmlentities($_POST['password']);
 
+  //$email = stripcslashes($email);
+  //$password = stripcslashes($password);
+  //$email = $mysqli -> real_escape_string($email);
+  //$password = $mysqli -> real_escape_string($password);
+
   // Connexion :
   require_once("param.inc.php");
   $mysqli = new mysqli($host, $login, $passwd, $dbname);
   if ($mysqli->connect_error) {
       die('Erreur de connexion (' . $mysqli->connect_errno . ') '
               . $mysqli->connect_error);
+  }else{
+    $result = $mysqli->query("SELECT * FROM user WHERE email = '$email'");
+    if(!$result){
+      die('Echec de la requête : '.$mysqli->error);
+    }elseif($result->num_rows == 0){
+      echo '<p>Aucun compte relié à cette email</p>';
+    }else{
+      $hash = $mysqli->query("SELECT password FROM user WHERE email = '$email'");
+      if(password_verify('$password', $hash)){
+        echo 'Connexion réussie !';
+        header('Location: account.php');
+      }
+      else{
+        echo '<p>Mot de passe non valide (ou juste code non fonctionnel mais chuttttt</p>';
+        header('Location: connexion.php');
+      }
+    }
   }
-
-  // Faire ici la connexion :
-
-  // Redirection vers la page d'accueil :
-  header('Location: index.php');
-
-
+ 
 ?>
