37f3e7a1ada0a0ae24ef71fea45265d9039a83be
Hugo Patry <hugo.patry@groupe-esigelec.org>
2022-09-28 09:59:16 +0200

    update bdd

--- bdd/user.sql
+++ bdd/user.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Ven 23 Septembre 2022 à 12:08
+-- Généré le :  Mer 28 Septembre 2022 à 07:58
 -- Version du serveur :  5.7.11
 -- Version de PHP :  7.0.3
 
@@ -45,7 +45,8 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`id`, `nom`, `prenom`, `email`, `password`, `role`, `active`, `creation_date`, `company`) VALUES
 (1, 'test1', 'test1', 'test@test.test', '$2y$11$JW3PHwCtZMAhb8SvFAQ4sOeFYsvQGRW7Hnq4IfJ.JU5u7cCWXtrQK', 'vis', 0, '2022-09-18 12:58:48', 'test'),
 (8, 'test2', 'test2', 'test2@test.test', '$2y$11$eDojpBF0a832LQgm3zlZ6O6owyTtHtkGwWrMLleC3g22N1qbrV1gq', 'vis', 0, '2022-09-20 19:45:58', 'test'),
-(10, 'test4', 'test4', 'test4@test.test', '$2y$11$aQnYhz3/ptakXhEu18u.Y.802EhcDo9NZn/57ssC6T1oH7R6L71n6', 'vis', 0, '2022-09-22 14:41:38', 'test');
+(10, 'test4', 'test4', 'test4@test.test', '$2y$11$aQnYhz3/ptakXhEu18u.Y.802EhcDo9NZn/57ssC6T1oH7R6L71n6', 'vis', 0, '2022-09-22 14:41:38', 'test'),
+(11, 'Admin', 'istrateur', 'admin.admein@ad.min', '$2y$11$72a6mEqeCx1sIpBHh/h49eyNV15lhlrj3e6NRBGlAkAau1HnSim1.', 'adm', 1, '2022-09-23 13:13:57', 'admin');
 
 --
 -- Index pour les tables exportées
@@ -66,7 +67,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
